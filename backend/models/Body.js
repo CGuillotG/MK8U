227b2eb6b82046da6b771ddf46b270d08812e98c
CGuillotG <c.guillot@outlook.com>
2019-07-12 11:19:40 -0500

    Added kart DB models

---
+++ backend/models/Body.js
@@ -0,0 +1,66 @@
+let mongoose = require('mongoose')
+let Schema = mongoose.Schema
+
+let bodySchema = new Schema(
+    {
+        body: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        speedGround: {
+            type: Number,
+            required: true
+        },
+        speedWater: {
+            type: Number,
+            required: true
+        },
+        speedAir: {
+            type: Number,
+            required: true
+        },
+        speedAntiGravity: {
+            type: Number,
+            required: true
+        },
+        acceleration: {
+            type: Number,
+            required: true
+        },
+        weight: {
+            type: Number,
+            required: true
+        },
+        handlingGround: {
+            type: Number,
+            required: true
+        },
+        handlingWater: {
+            type: Number,
+            required: true
+        },
+        handlingAir: {
+            type: Number,
+            required: true
+        },
+        handlingAntiGravity: {
+            type: Number,
+            required: true
+        },
+        traction: {
+            type: Number,
+            required: true
+        },
+        miniTurbo: {
+            type: Number,
+            required: true
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+)
+
+module.exports = mongoose.model('Body', bodySchema)
