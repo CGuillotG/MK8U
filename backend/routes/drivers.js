98922ea684635cd9af71e8083ea8002defbdd3f2
CGuillotG <c.guillot@outlook.com>
2019-07-25 12:59:02 -0500

    changed drivers api from id to name

--- backend/routes/drivers.js
+++ backend/routes/drivers.js
@@ -2,11 +2,17 @@ const express = require('express')
 const router = express.Router()
 let Driver = require('../models/Driver')
 
-router.get('/:id', (req, res, next) => {
-    Driver.findById(req.params.id)
-        .then(driver => res.json(driver))
+router.get('/:name', (req, res, next) => {
+    Driver.find({ driver: req.params.name })
+        .then(driver => res.json(driver.length === 0 ? {} : driver[0]))
         .catch(e => next(e))
 })
+
+// router.get('/:id', (req, res, next) => {
+//     Driver.findById(req.params.id)
+//         .then(driver => res.json(driver))
+//         .catch(e => next(e))
+// })
 
 router.get('/', (req, res, next) => {
     Driver.find()
