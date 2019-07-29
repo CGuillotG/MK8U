b4eda70fd3cc27f0f804db8b1162bb5e3c15a619
CGuillotG <c.guillot@outlook.com>
2019-07-29 10:49:34 -0500

    Fixed app.js

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: '../.env' })
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const express = require('express')
-//const favicon = require('serve-favicon')
+const favicon = require('serve-favicon')
 const mongoose = require('mongoose')
 const logger = require('morgan')
 const path = require('path')
@@ -14,12 +14,8 @@ const cors = require('cors')
 
 mongoose
     .connect(process.env.DB, { useCreateIndex: true, useNewUrlParser: true })
-    .then(x => {
-        console.log(`Connected to Mongo! Database name: "${x.connections[0].db.databaseName}"`)
-    })
-    .catch(err => {
-        console.error('Error connecting to mongo', err)
-    })
+    .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].db.databaseName}"`))
+    .catch(err => console.error('Error connecting to mongo', err))
 
 const app_name = require('./package.json').name
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`)
@@ -30,7 +26,7 @@ const app = express()
 app.use(
     cors({
         origin: true,
-        // origin: [process.env.PUBLIC_URL],
+        // origin: [process.env.REACT_APP_ENDPOINT],
         credentials: true
     })
 )
@@ -47,7 +43,7 @@ app.use(
         secret: process.env.SECRET,
         resave: false,
         saveUninitialized: true,
-        cookie: { httpOnly: true, maxAge: 2419200000 /*30 days*/ },
+        cookie: { httpOnly: true, maxAge: 2592000000 /*30 days*/ },
         store: new MongoStore({
             mongooseConnection: mongoose.connection,
             ttl: 24 * 60 * 60 // 1 day
@@ -58,8 +54,9 @@ app.use(
 //passport config
 app.use(passport.initialize())
 app.use(passport.session())
-// app.use(express.static(path.join(__dirname, 'public')))
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 const auth = require('./routes/auth')
 const drivers = require('./routes/drivers')
